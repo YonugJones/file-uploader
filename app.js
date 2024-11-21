4a2b79cfcae55d220693157d181bb4b67785404a
YonugJones <peterckerfoot@gmail.com>
2024-11-21 15:38:59 -0600

    Added routes for auth and index

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(
   expressSession({
     cookie: {
@@ -30,7 +31,7 @@ app,use(passport.expressSession());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const router = Router();
+const authController = require('../controllers/authController');
+
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
+router.get('/signup', authController.getSignup);
+router.post('/signup', authController.postSignup);
+router.get('/logout', authController.logout);
+
+module.exports = router;
