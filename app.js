0686025cf1513b07bbf2f79aa3fbf9b0b57d419a
YonugJones <peterckerfoot@gmail.com>
2024-11-21 15:23:54 -0600

    created server.js file also added more app config middleware

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const expressSession = require('express-session');
 const passport = require('middlewares/passport');
@@ -25,6 +24,8 @@ app.use(
     )
   })
 );
+app.use(passport.initialize());
+app,use(passport.expressSession());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -32,7 +33,4 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-})
+module.exports = app;
